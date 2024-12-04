7b3033fb0abaab12595759f087457a8fe7b67023
group3discretemath <dfira.lubnaziza@gmail.com>
2024-12-05 00:39:33 +0700

    hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import plotly.figure_factory as ff
 import pandas as pd
 import numpy as np
 
@@ -32,22 +31,6 @@ chart_data = pd.DataFrame(
 st.line_chart(chart_data)
 
 
-
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
 
 # radio widget to take inputs from mulitple options
 genre = st.radio(
